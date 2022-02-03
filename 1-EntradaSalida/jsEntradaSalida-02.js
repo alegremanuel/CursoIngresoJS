b1abf73ba82fae4801fd804c1cd9c25ead1307a8
alegremanuel <manueelalegre@gmail.com>
2022-02-03 16:05:30 -0300

    ejercicio 2 hecho

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,17 @@
 /*
+alegre victor manuiel
+ejercicio 2
+
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+
+let nombre
+
+
+nombre = prompt ("Ingrese su nombre");
+
+	alert(nombre);
 }
 

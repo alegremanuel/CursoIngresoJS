56404e643d2627a8f7865a907a93f81bdf5e1989
alegremanuel <manueelalegre@gmail.com>
2022-02-03 17:28:28 -0300

    ejercicio 3 hecho

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,18 @@
 /*
+ejercicio 3
+alegre victor manuel
+
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+{ 
+	let nombreingresado;
+	
+	/*le pido que me traiga el valor de un documento en especifico
+	por su nombre*/
+	nombreingresado=document.getElementById("txtIdNombre").value;
+
+	alert(nombreingresado);
 
 }
 

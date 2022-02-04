14253e96c25f505b32dc630fa31bc169e3e04e38
alegremanuel <manueelalegre@gmail.com>
2022-02-04 16:40:04 -0300

    ejercicio 6 hecho

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -7,16 +7,20 @@ function sumar()
 	let numero1;
 	let numero2;
 	let resultado;
+	let mensaje;
 
 	numero1 = document.getElementById("txtIdNumeroUno").value;
-	numero2 = document.getElementById("txtIdNumeroDos").value:
+	numero2 = document.getElementById("txtIdNumeroDos").value;
 
 	numero1 = parseInt(numero1);
 	numero2 = parseInt(numero2);
 	
-	resultado = (numero1 + numero2);
+	resultado = numero1 + numero2;
 
-	alert(resultado);
+	mensaje = "el resultado es " + resultado;
+
+	alert(mensaje);
+
 
 }
 

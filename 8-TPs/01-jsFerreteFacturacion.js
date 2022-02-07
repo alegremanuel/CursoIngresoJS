24c8615bcd4c8589b5e3c99ba72c9afdd7f1212b
alegremanuel <manueelalegre@gmail.com>
2022-02-07 19:09:10 -0300

    tp1 en proceso

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,6 +5,25 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	let precio1
+	let precio2
+	let precio3
+	let resultado
+	let mensaje
+
+	precio1 = document.getElementById("txtIdPrecioUno").value;
+	precio2 = document.getElementById("txtIdPrecioDos").value;
+	precio3 = document.getElementById("txtIdPrecioTres").value;
+
+	precio1 = parseInt(precio1);
+	precio2 = parseInt(precio2);
+	precio3 = parseInt(precio3);
+
+	resultado = precio1 + precio2 + precio3;
+
+	mensaje = "el precio total es " + resultado
+
+	alert = (mensaje);
 	
 }
 function Promedio () 

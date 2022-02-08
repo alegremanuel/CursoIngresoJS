70ec5ce0978d41067d5636e14033efb733adf9e7
alegremanuel <manueelalegre@gmail.com>
2022-02-08 00:27:53 -0300

    ejercicio 9 hecho

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -12,12 +12,16 @@ function mostrarAumento()
 	let aumento;
 	let sueldoaumentado
 
+	//ingreso monto del sueldo
 	sueldo = parseInt(document.getElementById("txtIdSueldo").value);
 
+	//calculo el 10% del sueldo
 	aumento = sueldo * 0.1;
 
+	//calculo el resultado final de sueldo con aumento
 	sueldoaumentado = sueldo + aumento
 	
+	//muestro en cuadro resultado el monto final
 	document.getElementById("txtIdResultado").value = sueldoaumentado;
 
 

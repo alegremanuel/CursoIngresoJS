9f2922550a4c9e44bfca189835a5c4c77518123e
alegremanuel <manueelalegre@gmail.com>
2022-02-09 17:32:06 -0300

    ejercicio if4 hecho

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	//tomo la edad 
+	edad=parseInt(document.getElementById("txtIdEdad").value);
+
+	//aplico condicion
+	if(edad >= 13 && edad <= 17){
+
+	//muestro mensaje si cumple la condicion 
+	alert("usted es adolescente");
+}
+
 
 
 }//FIN DE LA FUNCIÓN

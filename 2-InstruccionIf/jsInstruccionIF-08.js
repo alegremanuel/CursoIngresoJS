019ef23b467c3d2772e341153ec703e7bd9ec524
alegremanuel <manueelalegre@gmail.com>
2022-02-10 17:48:13 -0300

    ejercicio if9 hecho

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,14 +1,25 @@
 function mostrar()
 {
-	let edad
-	let estadocivil
+	let edad;
+	let estadocivil;
 
 	edad=parseInt(document.getElementById("txtIdEdad").value);
 	estadocivil=document.getElementById("estadoCivil").value;
 
-	if(edad>=18 && estadocivil == "Soltero")
+	if(edad>=18 && estadocivil == "Soltero");
 	{
-	alert("Es soltero y no es menor")
+	alert("Es soltero y no es menor");
 	}
 
 }//FIN DE LA FUNCIÓN
+
+
+/*
+const a=10
+const b=5
+const c=8
+
+if(a<b && a<c){alert("a es el menor")}
+	else if (b<a && b<c){alert("b es el menor")}
+		else{ alert("c es el menor")}
+*/

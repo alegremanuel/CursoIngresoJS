d5a274afbdce429c8bdd44bd7cdb765e700a909c
alegremanuel <manueelalegre@gmail.com>
2022-02-11 18:16:00 -0300

    ejercicio switch 9 proceso

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,31 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let preciobase;
+	let descuento;
+	let aumento
+	let preciofinal;
+	let estacion;
+	let destino;
+	let mensaje;
 
+	estacion=document.getElementById("txtIdEstacion").value;
+	destino=document.getElementById("txtIdDestino").value;
+
+	preciobase=15000;
+
+if(estacion == "Invierno"){
+	switch(destino){
+		case "Bariloche":
+			aumento=preciobase*0.20;
+				preciofinal=preciobase+aumento;
+					mensaje=preciofinal;
+						alert(mensaje);
+
+		}	
+}
+
+
+
+
+	
 }//FIN DE LA FUNCIÓN

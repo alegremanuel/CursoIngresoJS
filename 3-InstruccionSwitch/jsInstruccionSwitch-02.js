3f2ff52e1b1471e290bfa9cce7fe725d42cba280
alegremanuel <manueelalegre@gmail.com>
2022-02-11 16:07:25 -0300

    ejercicio switch 2 hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,47 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes;
 
+	mes=document.getElementById("txtIdMes").value;
 
+	switch(mes){
 
+		case "Enero":
+			alert("falta para el invierno");
+				break
+		case "Febrero":
+			alert("falta para el invierno");
+				break
+		case "Marzo":
+			alert("falta para el invierno");
+				break
+		case "Abril":
+			alert("falta para el invierno");
+				break
+		case "Mayo":
+			alert("falta para el invierno");
+				break
+		case "Junio":
+			alert("falta para el invierno");
+				break
+		case "Septiembre":
+				break
+			alert("ya pasamos el frio, ahora el calor");
+				break
+		case "Octubre":
+			alert("ya pasamos el frio, ahora el calor");
+				break
+		case "Noviembre":
+			alert("ya pasamos el frio, ahora el calor");
+				break
+		case "Diciembre":
+			alert("ya pasamos el frio, ahora el calor");
+				break
+		case "Julio":
+			alert("Abrigate que hace frio");
+				break
+		case "Agosto":
+			alert("Abrigate que hace frio");
+		}
 
 }//FIN DE LA FUNCIÓN

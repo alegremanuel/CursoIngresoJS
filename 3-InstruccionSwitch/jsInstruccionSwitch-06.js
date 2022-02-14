c5c79515e7daf2c5542d05e1b875a0e7998b66f8
alegremanuel <manueelalegre@gmail.com>
2022-02-14 13:59:01 -0300

    ejercicio switch 6 proceso

--- 3-InstruccionSwitch/jsInstruccionSwitch-06.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-06.js
@@ -1,7 +1,22 @@
 function mostrar()
 {
-	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
+	let hora;
+
+	hora=document.getElementById("txtIdHora").value;
+
+	switch(hora>=7 && hora<=11){
+		case true:
+			alert("Es de mañana");
+				break;
+			}
+
+		switch(hora>=12 && hora<=19){
+			case true:
+				alert("Es de tarde");
+					break
+		default:
+			alert("Es de noche");
+			}
+
 
 }//FIN DE LA FUNCIÓN

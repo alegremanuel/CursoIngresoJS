0efa667887188f54a06b663464187baa770fdf15
alegremanuel <manueelalegre@gmail.com>
2022-02-14 12:30:14 -0300

    ejercicio switch 9 hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -2,7 +2,7 @@ function mostrar()
 {
 	let preciobase;
 	let descuento;
-	let aumento
+	let porcentaje
 	let preciofinal;
 	let estacion;
 	let destino;
@@ -16,16 +16,100 @@ function mostrar()
 if(estacion == "Invierno"){
 	switch(destino){
 		case "Bariloche":
-			aumento=preciobase*0.20;
-				preciofinal=preciobase+aumento;
-					mensaje=preciofinal;
+			porcentaje=preciobase*0.20;
+				preciofinal=preciobase+porcentaje;
+					mensaje="el precio de su viaje es de $" + preciofinal;
 						alert(mensaje);
 
 		}	
 }
 
+if(estacion == "Invierno"){
+	switch(destino){
+		case "Cataratas":
+		case "Cordoba":
+			porcentaje=preciobase*0.10;
+				preciofinal=preciobase-porcentaje;
+					mensaje="el precio de su viaje es de $" +preciofinal;
+						alert(mensaje);
 
+		}	
+}
 
+if(estacion == "Invierno"){
+	switch(destino){
+		case "Mar del plata":
+			porcentaje=preciobase*0.20;
+				preciofinal=preciobase-porcentaje;
+					mensaje="el precio de su viaje es de $" +preciofinal;
+						alert(mensaje);
 
+		}	
+}
 	
+if(estacion == "Verano"){
+	switch(destino){
+		case "Bariloche":
+			porcentaje=preciobase*0.20;
+				preciofinal=preciobase-porcentaje;
+					mensaje="el precio de su viaje es de $" +preciofinal;
+						alert(mensaje);
+		}	
+}
+
+if(estacion == "Verano"){
+	switch(destino){
+		case "Cordoba":
+		case "Cataratas":
+			porcentaje=preciobase*0.10;
+				preciofinal=preciobase+porcentaje;
+					mensaje="el precio de su viaje es de $" +preciofinal;
+						alert(mensaje);
+		}	
+}
+
+if(estacion == "Verano"){
+	switch(destino){
+		case "Mar del plata":
+			porcentaje=preciobase*0.20;
+				preciofinal=preciobase+porcentaje;
+					mensaje="el precio de su viaje es de $" +preciofinal;
+						alert(mensaje);
+		}	
+}
+
+if(estacion == "Otoño"){
+	switch(destino){
+		case "Bariloche":
+		case "Cataratas":
+		case "Mar del plata":
+			porcentaje=preciobase*0.10;
+				preciofinal=preciobase+porcentaje;
+					mensaje="el precio de su viaje es de $" +preciofinal;
+						alert(mensaje);
+					break;	
+		default:
+			mensaje="el precio de su viaje es de $" +preciobase;
+			alert(mensaje);
+		}
+		
+}
+
+if(estacion == "Primavera"){
+	switch(destino){
+		case "Bariloche":
+		case "Cataratas":
+		case "Mar del plata":
+			porcentaje=preciobase*0.10;
+				preciofinal=preciobase+porcentaje;
+					mensaje="el precio de su viaje es de $" +preciofinal;
+						alert(mensaje);
+					break;	
+		default:
+			mensaje="el precio de su viaje es de $" +preciobase;
+			alert(mensaje);
+		}
+		
+}
+
 }//FIN DE LA FUNCIÓN

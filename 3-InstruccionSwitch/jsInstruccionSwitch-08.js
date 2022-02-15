3ff07614848658d9dfef577dfdc500d284cac88e
alegremanuel <manueelalegre@gmail.com>
2022-02-15 18:22:36 -0300

    ejercicio switch 8 hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,29 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
+
+	destino=document.getElementById("txtIdDestino").value;
+
+	switch(destino){
+		case "Bariloche":
+			alert("Hace frio");
+				break;
+	}
+	
+	switch(destino){
+		case "Cataratas":
+			alert("Hace calor");
+				break;
+	}
+	switch(destino){
+		case "Mar del plata":
+			alert("Hace calor");
+				break;
+	}
+	switch(destino){
+		case "Ushuaia":
+			alert("Hace Frio");
+				break;
+	}
 
 }//FIN DE LA FUNCIÓN

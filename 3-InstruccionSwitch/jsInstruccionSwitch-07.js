43968615f7b74fb338e187dc09a441b9175638f8
alegremanuel <manueelalegre@gmail.com>
2022-02-15 18:17:26 -0300

    ejercicio switch 7 hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,29 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
+
+	destino=document.getElementById("txtIdDestino").value;
+
+	switch(destino){
+		case "Bariloche":
+			alert("Se encuentra al sur de nuestro pais");
+				break;
+	}
+	switch(destino){
+		case "Cataratas":
+			alert("Se encuentra al norte de nuestro pais");
+				break;
+	}		
+	switch(destino){
+		case "Mar del plata":
+			alert("Se encuentra al este de nuestro pais");
+				break;
+	}		
+	switch(destino){
+		case "Ushuaia":
+			alert("Se encuentra al sur de nuestro pais");
+				break;
+	}		
+
 
 }//FIN DE LA FUNCIÓN
